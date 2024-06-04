ff9a8126d25945b506fe7dd301fd5ea88a64b62a
mariamtariqq <mariam.cs147@gmail.com>
2024-06-04 17:51:34 +0500

    sidebar done

--- script.js
+++ script.js
@@ -1,19 +1,27 @@
-// document.getElementById('hamburger').addEventListener("click", showBar);
-
-// function showBar() {
-//     var element = document.getElementById("navbar");
-//     element.classList.toggle("hidden");
-// }
-
 const hamburger = document.getElementById("hamburger");
 const navbar = document.getElementById("navbar");
 
-hamburger.addEventListener("click", () => {
-  // navbar.classList.toggle("hidden");
-  if (navbar.classList.contains("yesHidden")) {
+let sidebar = document.querySelector(".sidebar");
+
+document.querySelector('#hamburger').onclick = e => {
+    if (navbar.classList.contains("yesHidden")) {
     navbar.classList.remove("yesHidden");
     navbar.classList.add("notHidden");
   } else {
+    navbar.classList.remove("notHidden");
+    navbar.classList.add("yesHidden");
+  }
+  e.stopPropagation()
+};
+
+document.querySelector('#close').onclick = e => {
+  navbar.classList.remove("notHidden");
+  navbar.classList.add("yesHidden");
+  e.stopPropagation()
+};
+
+document.body.addEventListener("click", e => {
+  if (!navbar.contains(e.target)) {
     navbar.classList.remove("notHidden");
     navbar.classList.add("yesHidden");
   }
